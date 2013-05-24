9649516705a7efb67107bf9252c6eeb7bb3f9a99
cmawhorter <cory.mawhorter@gmail.com>
2013-05-23 19:38:20 -0700

    move to subdir

--- src/client/StamprApi.php
+++ src/client/StamprApi.php
@@ -5,7 +5,7 @@ use Guzzle\Plugin\CurlAuth\CurlAuthPlugin;
 
 class StamprApi extends Client
 {
-	public function __construct($user, $password, $endpoint='https://stam.pr/api') 
+	public function __construct($user, $password, $endpoint='https://stam.pr/api')
 	{
 		parent::__construct($endpoint);
 		$authPlugin = new CurlAuthPlugin($user, $password);
